e135dd7ae4a68c492635236e680ed0a114a79267
hungtv <hung161203@gmail.com>
2023-12-03 21:40:47 +0700

    sum_array

---
+++ SUM_ARRAY.cpp
@@ -0,0 +1,14 @@
+//CPP
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,t; cin>>n;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>t;
+        sum+=t;
+    }
+    cout<< sum;
+    return 0;
+}
